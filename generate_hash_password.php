3d2a1ab5dbb2173dc6fd3ecbd0e4e7ca74cb333f
sumaiyakhtar2003 <sumaiya.akhtar@g.bracu.ac.bd>
2025-01-07 15:34:32 +0600

    Update generate_hash_password.php

--- generate_hash_password.php
+++ generate_hash_password.php
@@ -1,6 +1,5 @@
-<?php
-// Example: Hash a plain-text password
-$password = 'uems123'; // Replace with your current password
+<?php 
+$password = 'uems123'; 
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 // Output the hashed password
